6dd006b393235c28c4fe3f52787988dad3f77c1f
Kalea <41207187+KaleaLoftis@users.noreply.github.com>
2023-09-28 14:06:35 -0700

    Finished JS ICA

--- scripts.js
+++ scripts.js
@@ -30,3 +30,34 @@ for (let span of ltBluSpan){
 for (let span of mdBluSpan){
     span.style.color = "#3C5E73";
 }
+
+
+//CSS Selector (as a group)--------------------------------
+//
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+
+let svgs = document.querySelectorAll("#svgs svg");
+
+//console.log(svgs);
+//iterate through and change colors of each svg in the collection
+for (let i = 0; i < svgs.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
+
+// individual element with css selector -------------
+// get the first span on page with a class of bold
+document.querySelector(".bold").style.color = "#7C9EA6";
+
+
+//changing node content --------------------------------------
+// changing content
+document.querySelector(".content_list li:first-of-type").textContent = "this is new list item text";
+// adding content 
+document.querySelectorAll(".content_list li")[2].innerHTML += "<strong> Kalea Loftis</strong>";
+
+
+//remove an attribute -----------------------------------
+document.querySelector("#remove a").removeAttribute("hidden");
+
+
